f6844ade45ad7bdb647c71603d0ba5cf4c75677a
rashmibimashaa <rashmibimashaa@gmail.com>
2026-08-22 19:42:35 +0530

    chore: remove temporary dashboard test and endpoint

--- src/main/java/com/hexaco/hrms/rest/DashboardController.java
+++ src/main/java/com/hexaco/hrms/rest/DashboardController.java
@@ -40,9 +40,4 @@ public class DashboardController {
     public ResponseEntity<DirectorDashboardDto> getDirectorDashboard() {
         return ResponseEntity.ok(directorDashboardService.getDirectorDashboard());
     }
-
-    @GetMapping("/test")
-    public ResponseEntity<DashboardAnalyticsDto> getTestAnalytics() {
-        return ResponseEntity.ok(dashboardService.getAnalytics());
-    }
 }
